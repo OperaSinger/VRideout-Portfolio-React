679e898d757ef7f33fff97c59238d10bb4c4eef9
Nigel Tufnel <tenorideout@gmail.com>
2018-12-06 11:07:05 -0500

    trying to fix Router issues

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Route } from "react-router-dom";
 import About from "./about";
 import Projects from "./projects";
 import Contact from "./contact";
-// import Testing from "./testing";
 
 const Content = () => {
   return (
-    // <BrowserRouter basename={process.env.PUBLIC_URL}>
-
-    <BrowserRouter baseUrl="/VRideout-Portfolio-React">
-      <Switch>
-        <Route exact path={`${process.env.PUBLIC_URL}/`} component={About} />
-        <Route
-          path={`${process.env.PUBLIC_URL}/projects`}
-          component={Projects}
-        />
-        <Route path={`${process.env.PUBLIC_URL}/contact`} component={Contact} />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <Route exact path={`${process.env.PUBLIC_URL}/`} component={About} />
+      <Route path={`${process.env.PUBLIC_URL}/projects`} component={Projects} />
+      <Route path={`${process.env.PUBLIC_URL}/contact`} component={Contact} />
+    </div>
   );
 };
 
